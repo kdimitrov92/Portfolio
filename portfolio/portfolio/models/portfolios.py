4804f0420c8978ac939e516a2fac53a34ebe792c
kdimitrov92 <kdimitrov92@gmail.com>
2015-06-07 16:22:48 +0300

    Adding a portfolio model;

---
+++ portfolio/portfolio/models/portfolios.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from portfolio.models.photos import Photo
+
+
+class Portfolio(models.Model):
+    owner = models.OneToOneField(User, primary_key=True)
+    description = models.CharField(max_length=300)
+    photo = models.OneToOneField(Photo)
