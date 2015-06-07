d03b8bb51a19cc49d1ea09537a0a3d91e1301326
kdimitrov92 <kdimitrov92@gmail.com>
2015-06-07 15:26:26 +0300

    Added an image field to the Photo model;

--- portfolio/portfolio/models/photos.py
+++ portfolio/portfolio/models/photos.py
@@ -14,8 +14,9 @@ class Photo(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=200)
-    owner = models.ForeignKey(User, related_name='photo_owner')
-    album = models.ManyToManyField(Album, related_name='photo_album', blank=True)
-    original = models.ForeignKey('self', related_name='original_photo', blank=True)
+    owner = models.ForeignKey(User, related_name='photos')
+    album = models.ManyToManyField(Album, related_name='photos', blank=True)
+    original = models.ForeignKey('self', related_name='alternative_versions', blank=True)
+    image = models.ImageField(null=False, blank=False)
 
     tags = TaggableManager()
